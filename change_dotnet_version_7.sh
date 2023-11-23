786f71dcff657373871d2690d80faa2520f2e54f
L <401419353@qq.com>
2023-11-23 10:02:23 +0800

    修复 dotnet 版本批量修改脚本中的运行错误

--- change_dotnet_version_7.sh
+++ change_dotnet_version_7.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-# 查找所有.csproj文件并替换dotnet版本为8.0
+# 查找所有.csproj文件并替换dotnet版本为7.0
 
-find . -name "*.csproj" -type f -exec sed -i 's/<TargetFramework>net[0-9.]*<\/TargetFramework>/<TargetFramework>net7.0<\/TargetFramework>/g' {} +
+find . -name '*.csproj' -exec sed -i '' 's/net[0-9]\.[0-9]/net7.0/g' {} \;
--- change_dotnet_version_8.sh
+++ change_dotnet_version_8.sh
@@ -2,4 +2,4 @@
 
 # 查找所有.csproj文件并替换dotnet版本为8.0
 
-find . -name "*.csproj" -type f -exec sed -i 's/<TargetFramework>net[0-9.]*<\/TargetFramework>/<TargetFramework>net8.0<\/TargetFramework>/g' {} +
+find . -name '*.csproj' -exec sed -i '' 's/net[0-9]\.[0-9]/net8.0/g' {} \;
